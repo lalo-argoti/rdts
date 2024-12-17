858f2be4a70d5158d8ff4da1197b49284af571c2
Your Name <carlos.e.argoti@gmail.com>
2024-12-17 14:35:42 -0500

    2:53

--- frontendANGLR/src/app/login/login-form.component.ts
+++ frontendANGLR/src/app/login/login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-login-form',
